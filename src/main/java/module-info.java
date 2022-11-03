2c315979d2ee193172e785d3fb41a3c783432b3d
RehasLiamsi <nsaher91@gmail.com>
2022-11-03 13:12:33 +0100

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module se.iths.tt.javafxtt {
 
     opens se.iths.tt.javafxtt to javafx.fxml;
     exports se.iths.tt.javafxtt;
+    exports se.iths.tt.javafxtt.labb3;
+    exports se.iths.tt.javafxtt.labb3.controller;
 }
